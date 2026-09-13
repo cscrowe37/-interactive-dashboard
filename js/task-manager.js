a7e0f46b99e97d3542ae6194f035143b9dc94391
cscrowe37 <cscrowe@my.waketech.edu>
2026-09-13 14:50:13 -0400

    debug3

--- js/task-manager.js
+++ js/task-manager.js
@@ -21,7 +21,7 @@ function weeklyGoal(userName,dailyGoal,bonusTasks) {
     let output = ("User: " + userName + "<br>");
     output += ("Total Weekly Goal:  "+ totalGoal);
     document.getElementById("goal-message").innerHTML = output;
-
+    preventDefault();
 
 }
 

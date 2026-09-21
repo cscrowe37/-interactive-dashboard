1cad2c8ef300f437a3666d2bc13ebb3167a62fae
cscrowe37 <cscrowe@my.waketech.edu>
2026-09-21 19:52:44 -0400

    Add metric-converter.js for unit conversion
    Implement metric and imperial conversion logic with user input.

---
+++ metric-converter.js
@@ -0,0 +1,41 @@
+// Display info
+console.log("Code converts Imperial to Metric and vice versa");
+console.log("Imperial measurements: inch, foot, yard, mile");
+console.log("Metric measurements: centimeter, meter, kilometer");
+
+
+function convertfunction(){
+// Input
+var number = document.getElementById("numbervalue");
+var direction = document.getElementsByTagName("Measurments");
+let input_value = parseFloat(number)
+
+
+// Convert
+
+    if (direction === "in>cm") {
+        input_value = input_value * 2.54;
+    } else if (direction === "ft>cm") {
+        input_value = input_value * 30.48;
+    } else if (direction === "yd>m") {
+        input_value = input_value * 0.91;
+    } else if (direction === "mi>km") {
+        input_value = input_value * 1.61;
+    } 
+    else if (currentType === "cm>in") {
+        input_value = input_value * 0.39;
+    } else if (currentType === "cm>ft") {
+        input_value = input_value * 0.0328;
+    } else if (currentType === "m>yd") {
+        input_value = input_value * 1.09;
+    } else if (currentType === "km>mir") {
+        input_value = input_value * 0.62;
+    
+}
+
+// Output
+console.log(input_value);
+
+}
+const buttonForConverter = document.getElementById("startconversion")
+buttonForConverter.addEventListener("click",convertfunction)
